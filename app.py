e780ae6459c4b845b268723a3ff1abe0f72e51be
Animeshka39 <sergiosasa9@gmail.com>
2023-05-31 11:50:01 +0300

    engine_hp

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, jsonify, request
+from flask import Flask, render_template, jsonify, request, redirect
 import psycopg2
 import psycopg2.extras
 
@@ -7,7 +7,7 @@ app = Flask(__name__)
 app.secret_key = "caircocoders-ednalan"
 
 DB_HOST = "localhost"
-DB_NAME = "sampledb"
+DB_NAME = "sampledb0"
 DB_USER = "postgres"
 DB_PASS = "51654372985"
 
@@ -61,6 +61,8 @@ def carcharacteristics():
                 'brand_name': car['brand_name'],
                 'model_id': car['model_id'],
                 'car_model': car['car_model'],
+                'body_type': car['body_type'],
+                'number_doors': car['number_doors'],
                 'price_range': car['price_range'],
                 'year': car['year'],
                 'mileage': car['mileage'],
@@ -70,7 +72,9 @@ def carcharacteristics():
                 'technical_condition': car['technical_condition'],
                 'customs_cleared': car['customs_cleared'],
                 'driven_from': car['driven_from'],
+                'engine_size': car['engine_size'],
                 'engine_name': car['engine_name'],
+                'engine_hp': car['engine_hp'],
                 'location': car['location'],
                 'seller_id': car['seller_id']
             }
@@ -87,15 +91,18 @@ def filter_cars():
     brand_id = request.form.get('brand_id')
     model_id = request.form.get('model_id')
     transmission = request.form.get('transmission')
+    body_type = request.form.get('body_type')
+    number_doors = request.form.get('number_doors')
     min_price = request.form.get('min_price')
     max_price = request.form.get('max_price')
     min_year = request.form.get('min_year')
     max_year = request.form.get('max_year')
     min_mileage = request.form.get('min_mileage')  
     max_mileage = request.form.get('max_mileage')
-    engine_name = request.form.get('engine_name')   
+    engine_size = request.form.get('engine_size') 
+    engine_name = request.form.get('engine_name') 
+    engine_hp = request.form.get('engine_hp')   
     location = request.form.get('location')
-    
 
     query = "SELECT * FROM carmodels WHERE 1=1"
 
@@ -109,6 +116,12 @@ def filter_cars():
     if transmission:
         query += " AND transmission = %s"
         params.append(transmission)
+    if body_type:
+        query += " AND body_type = %s" 
+        params.append(body_type)
+    if number_doors:
+        query += " AND number_doors = %s" 
+        params.append(number_doors)
     if min_price:
         query += " AND price_range >= %s"
         params.append(min_price)
@@ -127,9 +140,15 @@ def filter_cars():
     if max_mileage:
         query += " AND mileage <= %s"
         params.append(max_mileage)
+    if engine_size:
+        query += " AND engine_size = %s"
+        params.append(engine_size)
     if engine_name:
         query += " AND engine_name = %s"
         params.append(engine_name)
+    if engine_hp:
+        query += " AND engine_hp = %s"
+        params.append(engine_hp)
     if location:
         query += " AND location = %s"
         params.append(location)  
@@ -142,25 +161,73 @@ def filter_cars():
     filtered_cars = []
     for car in cars:
         filtered_cars.append({
-                'car_photo': car['car_photo'],
-                
-                'model_id': car['model_id'],
-                'car_model': car['car_model'],
-                'price_range': car['price_range'],
-                'year': car['year'],
-                'mileage': car['mileage'],
-                'transmission': car['transmission'],
-                'fuel_type': car['fuel_type'],
-                'color': car['color'],
-                'technical_condition': car['technical_condition'],
-                'customs_cleared': car['customs_cleared'],
-                'driven_from': car['driven_from'],
-                'engine_name': car['engine_name'],
-                'location': car['location'],
-                'seller_id': car['seller_id']
+            'car_photo': car['car_photo'],
+            'model_id': car['model_id'],
+            'car_model': car['car_model'],
+            'body_type': car['body_type'],
+            'number_doors': car['number_doors'],
+            'price_range': car['price_range'],
+            'year': car['year'],
+            'mileage': car['mileage'],
+            'transmission': car['transmission'],
+            'fuel_type': car['fuel_type'],
+            'color': car['color'],
+            'technical_condition': car['technical_condition'],
+            'customs_cleared': car['customs_cleared'],
+            'driven_from': car['driven_from'],
+            'engine_size': car['engine_size'],
+            'engine_name': car['engine_name'],
+            'engine_hp': car['engine_hp'],
+            'location': car['location'],
+            'seller_id': car['seller_id']
         })
 
     return jsonify(filtered_cars)
+
+
+@app.route("/addcar", methods=["POST"])
+def add_car():
+    cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
+    
+    brand_id = request.form.get('brand_id')
+    car_model = request.form.get('car_model')
+    body_type = request.form.get('body_type')
+    number_doors = request.form.get('number_doors')
+    price_range = request.form.get('price_range')
+    year = request.form.get('year')
+    mileage = request.form.get('mileage')
+    transmission = request.form.get('transmission')
+    fuel_type = request.form.get('fuel_type')
+    color = request.form.get('color')
+    technical_condition = request.form.get('technical_condition')
+    customs_cleared = request.form.get('customs_cleared')
+    driven_from = request.form.get('driven_from')
+    engine_size = request.form.get('engine_size')
+    engine_name = request.form.get('engine_name')
+    engine_hp = request.form.get('engine_hp')
+    location = request.form.get('location')
+    seller_id = request.form.get('seller_id')
+    
+    query = "INSERT INTO carmodels (brand_id, car_model, body_type, number_doors, price_range, year, mileage, transmission, fuel_type, color, technical_condition, customs_cleared, driven_from, engine_size, engine_name, engine_hp, location, seller_id) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
+    
+    cur.execute(query, (brand_id, car_model, body_type, number_doors, price_range, year, mileage, transmission, fuel_type, color, technical_condition, customs_cleared, driven_from, engine_size, engine_name, engine_hp, location, seller_id))
+    conn.commit()
+    
+    return redirect('/')
+
+
+@app.route("/deletecar", methods=["POST"])
+def delete_car():
+    cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
+    
+    model_id = request.form.get('model_id')
+    
+    query = "DELETE FROM carmodels WHERE model_id = %s"
+    
+    cur.execute(query, [model_id])
+    conn.commit()
+    
+    return redirect('/')
 
 
 if __name__ == "__main__":
